498bd3380cfe40673728b453ce25a73ecdcbe2ad
baggiiiie <yingchao.dai22@gmail.com>
2025-03-11 20:30:34 +0800

    feat: update aerospace config

---
+++ aerospace/center_window_raycast.sh
@@ -0,0 +1,1 @@
+open -g raycast://extensions/raycast/window-management/center
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+
+# aerospace
+ln -s "$CURRENT_DIR/aerospace/.aerospace.toml" "$HOME/.aerospace.toml"
