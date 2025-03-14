aaf36b60dc5ac09c9a9fc1ed67ef7f9260005102
baggiiiie <yingchao.dai22@gmail.com>
2025-03-14 18:26:51 +0800

    keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,6 +11,18 @@ map("n", "p", '""p')
 
 vim.keymap.set({ "c", "n", "i", "x", "v" }, "<C-a>", "<Home>", { desc = "Move to beginning of line" })
 vim.keymap.set({ "c", "n", "i", "x", "v" }, "<C-e>", "<End>", { desc = "Move to end of line" })
+vim.keymap.set("n", "<cr>", "o<esc>k", { desc = "Create new line in normal mode" })
+vim.keymap.set("n", "<S-cr>", "O<esc>j", { desc = "Create new line in normal mode" })
+
+vim.keymap.set("n", "zl", function()
+  local scroll_amount = math.floor(vim.api.nvim_win_get_width(0) / 2)
+  return scroll_amount .. "zl"
+end, { noremap = true, expr = true, silent = true, desc = "Scroll left by half screen" })
+
+vim.keymap.set("n", "zh", function()
+  local scroll_amount = math.floor(vim.api.nvim_win_get_width(0) / 2)
+  return scroll_amount .. "zh"
+end, { noremap = true, expr = true, silent = true, desc = "Scroll right by half screen" })
 
 function CopyRelativePath()
   local filepath = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.")
