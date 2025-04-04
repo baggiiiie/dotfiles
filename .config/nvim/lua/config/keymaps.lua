590d111a87a55c514404ae827f42bd212fee8ea6
baggiiiie <yingchao.dai22@gmail.com>
2025-04-04 15:11:19 +0800

    nvim: add delete file keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -65,3 +65,13 @@ vim.keymap.set(
   ":Telescope marks mark_type=local<CR>",
   { noremap = true, silent = true, desc = "Find marks in current buffer" }
 )
+
+local function confirm_and_delete_buffer()
+  local confirm = vim.fn.confirm("Delete buffer and file?", "&Yes\n&No", 2)
+
+  if confirm == 1 then
+    os.remove(vim.fn.expand("%"))
+    vim.api.nvim_buf_delete(0, { force = true })
+  end
+end
+vim.keymap.set("n", "<leader>fd", confirm_and_delete_buffer)
