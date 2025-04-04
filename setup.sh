6e15e9a4ab94c29a6e802f62e872cf73e035b816
baggiiiie <yingchao.dai22@gmail.com>
2025-04-04 12:17:53 +0800

    brew: add install script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# Get platform type
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS-specific configurations
+  export PLATFORM="macOS"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux-specific configurations
+  export PLATFORM="Linux"
+else
+  # Fallback for other systems
+  export PLATFORM="Unknown"
+fi
+
 CURRENT_DIR=$(pwd)
 
-# aerospace
-ln -s "$CURRENT_DIR/aerospace/.aerospace.toml" "$HOME/.aerospace.toml"
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle --file="$CURRENT_DIR/Brewfile"
