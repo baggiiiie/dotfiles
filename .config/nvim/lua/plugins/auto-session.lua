191105945ce13120331ed67774a5a516cd879fad
baggiiiie <yingchao.dai22@gmail.com>
2025-04-09 21:11:27 +0800

    nvim: adjust auto session

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -34,10 +34,12 @@ return {
   opts = {
     -- ⚠️ This will only work if Telescope.nvim is installed
     -- The following are already the default values, no need to provide them if these are already the settings you want.
+    auto_restore_last_session = false, -- On startup, loads the last saved session if session for cwd does not exist
+    use_git_branch = true, -- Include git branch name in session name
     session_lens = {
       -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
       load_on_setup = true,
-      previewer = false,
+      previewer = true,
       mappings = {
         -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
         delete_session = { "i", "<C-D>" },
@@ -48,10 +50,28 @@ return {
       -- For all options, see: https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt#L112
       theme_conf = {
         border = true,
-        -- layout_config = {
-        --   width = 0.8, -- Can set width and height as percent of window
-        --   height = 0.5,
-        -- },
+        layout_strategy = vim.o.lines > 100 and "vertical" or "horizontal",
+        preview = { hide_on_startup = false },
+        layout_config = {
+          flex = { flip_columns = 80 },
+          horizontal = {
+            preview_width = 0.7, -- Increase preview width (0.5 = 50% of window width)
+            width = 0.95, -- Make the whole Telescope window larger
+            height = 0.85, -- Increase the height of the Telescope popup
+            prompt_position = "top",
+          },
+          vertical = {
+            preview_height = 0.5, -- Increase preview width (0.5 = 50% of window width)
+            width = 0.95, -- Make the whole Telescope window larger
+            height = 0.85, -- Increase the height of the Telescope popup
+            prompt_position = "top",
+          },
+        },
+        -- sorting_strategy = "ascending",
+        --   layout_config = {
+        --     width = 0.8, -- Can set width and height as percent of window
+        --     height = 0.5,
+        --   },
       },
     },
   },
