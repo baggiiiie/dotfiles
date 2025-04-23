238ebb405b919a525d8fd43b7133d7105afc31ca
baggiiiie <yc@yingchaoooo.com>
2025-04-23 20:49:31 +0800

    nvim: add refactoring

---
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,40 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    prompt_func_return_type = {
+      go = false,
+      java = false,
+      cpp = false,
+      c = false,
+      h = false,
+      hpp = false,
+      cxx = false,
+    },
+    prompt_func_param_type = {
+      go = false,
+      java = false,
+      cpp = false,
+      c = false,
+      h = false,
+      hpp = false,
+      cxx = false,
+    },
+    printf_statements = {},
+    print_var_statements = {},
+    show_success_message = true, -- shows a message with information about the refactor on success
+    -- i.e. [Refactor] Inlined 3 variable occurrences
+  },
+  config = function(_, opts)
+    require("refactoring").setup(opts)
+    if LazyVim.has("telescope.nvim") then
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("refactoring")
+      end)
+    end
+  end,
+}
