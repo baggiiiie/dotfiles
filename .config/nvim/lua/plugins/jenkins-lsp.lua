d40096e5ec1a7fa933ed19c23fb4289efc4412f8
baggiiiie <yc@yingchaoooo.com>
2025-04-28 22:57:12 +0800

    nvim: lsp config

---
+++ .config/nvim/lua/plugins/jenkins-lsp.lua
@@ -0,0 +1,7 @@
+-- https://www.jenkins.io/doc/book/pipeline/development/#neovim-nvim-jenkinsfile-linter-plugin
+return {
+  "ckipp01/nvim-jenkinsfile-linter",
+  requires = {
+    "nvim-lua/plenary.nvim",
+  },
+}
---
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -0,0 +1,9 @@
+-- https://lsp-zero.netlify.app/docs/tutorial.html
+
+return {
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },
+  -- { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/nvim-cmp" },
+}
