8fa4cf9e1f0668b85283036a8321603d17c4b4ba
baggiiiie <yc@yingchaoooo.com>
2025-05-27 14:05:03 +0800

    nvim: remove some stuff

--- .config/nvim/lua/plugins/lsp-zero.lua
+++
@@ -1,9 +0,0 @@
--- https://lsp-zero.netlify.app/docs/tutorial.html
-
-return {
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  { "neovim/nvim-lspconfig" },
-  -- { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/nvim-cmp" },
-}
