526dddd3497b368a5c0d6e1301bbd1cded131796
baggiiiie <yc@yingchaoooo.com>
2025-06-26 00:05:21 +0800

    nvim: chore

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- require("lspconfig").cucumber_language_server.setup()
--- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
--- local lspconfig = require("lspconfig")
--- lspconfig.gopls.setup({
---   settings = {
---     gopls = {
---       analyses = {
---         unusedparams = true,
---       },
---       staticcheck = true,
---       gofumpt = true,
---     },
---   },
--- })
 require("no-neck-pain")
 
--- vim.cmd([[
---     set runtimepath^=~/.vim runtimepath+=~/.vim/after
---     let &packpath = &runtimepath
---     source ~/.vimrc
--- ]])
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig/configs")
+
+lspconfig.gopls.setup({
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+})
