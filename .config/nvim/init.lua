94cb3c883d45baba8120c024dd947baa37d052e4
baggiiiie <yc@yingchaoooo.com>
2025-11-14 14:39:35 +0800

    nvim: update init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,19 +3,41 @@ require("config.lazy")
 -- require("no-neck-pain")
 
 -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
-local lspconfig = require("lspconfig")
+-- local lspconfig = require("lspconfig")
 -- local configs = require("lspconfig/configs")
+-- lspconfig.gopls.setup({
+--   settings = {
+--     gopls = {
+--       analyses = {
+--         unusedparams = true,
+--       },
+--       staticcheck = true,
+--       gofumpt = true,
+--     },
+--   },
+-- })
+--
+-- require("config.floaterm")
 
-lspconfig.gopls.setup({
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-    },
+-- https://ast-grep.github.io/guide/tools/editors.html#neovim
+require("lspconfig").ast_grep.setup({
+  -- these are the default options, you only need to specify
+  -- options you'd like to change from the default
+  cmd = { "ast-grep", "lsp" },
+  filetypes = {
+    "c",
+    "cpp",
+    "rust",
+    "go",
+    "java",
+    "python",
+    "javascript",
+    "typescript",
+    "html",
+    "css",
+    "kotlin",
+    "dart",
+    "lua",
   },
+  root_dir = require("lspconfig.util").root_pattern("sgconfig.yaml", "sgconfig.yml"),
 })
-
-require("config.floaterm")
