24f85e97d37d526a64666d300b1849fb8c4c2bd9
baggiiiie <yc@yingchaoooo.com>
2025-11-27 15:22:06 +0800

    tmux: update scripts

--- others/tmux-init-sesh.sh
+++ others/tmux-init-sesh.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+session_name="$1"
+session_path="$2"
+
 # Rename the first window to "editor"
-tmux rename-window -t 1 editor
+tmux rename-window -t "$session_name:1" -c "$session_path" editor
 
 # Create second window named "agents"
-tmux new-window -n agent
+tmux new-window -t "$session_name" -n agent -c "$session_path"
 
 # Create third window named "term"
-tmux new-window -n terminal
+tmux new-window -t "$session_name" -n terminal -c "$session_path"
 
-# Create third window named "jjui"
-tmux new-window -n jjui
+# Create fourth window named "jjui"
+tmux new-window -t "$session_name" -n jjui -c "$session_path"
 
-# Create fourth window named "server"
-tmux new-window -n server
+# Create fifth window named "server"
+tmux new-window -t "$session_name" -n server -c "$session_path"
 
 # Switch back to the first window (editor)
-tmux select-window -t 1
+tmux select-window -t "$session_name:1"
--- others/tmux-sessionizer.sh
+++ others/tmux-sessionizer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 REPO_DIR="$HOME/Desktop/repos"
+sesh_to_init=("eosctl" "coffeeee" "jjui")
 
 if [[ $# -eq 1 ]]; then
     session_name=$1
@@ -20,6 +21,13 @@ fi
 
 if ! tmux has-session -t="$session_name" 2>/dev/null; then
     tmux new-session -ds "$session_name" -c "$session_path"
+    # Run init script if session_name is in sesh_to_init array
+    for sesh in "${sesh_to_init[@]}"; do
+        if [[ "$session_name" == *"$sesh"* ]]; then
+            bash "$(dirname "$0")/tmux-init-sesh.sh" "$session_name" "$session_path"
+            break
+        fi
+    done
 fi
 
 tmux switch-client -t "$session_name"
