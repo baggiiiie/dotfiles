ef4a954a36a1d330597b9ebb52140f47a7959ce5
baggiiiie <yc@yingchaoooo.com>
2025-12-13 00:26:20 +0800

    osc54 copy, just write to pbcopy and osc54 i guess

--- others/yank.sh
+++ others/yank.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 # Save as ~/bin/yank or similar
-osc52() {
-    buf=$(cat)
-    len=$(echo -n "$buf" | wc -c)
-    max=74994
+# Usage: command | yank.sh
 
-    if [ $len -gt $max ]; then
-        echo "Input is too large for OSC 52" >&2
-        return 1
-    fi
+# Read from stdin
+buf=$(cat)
 
+# Write to pbcopy
+echo -n "$buf" | /usr/bin/pbcopy
+
+# Write to OSC52
+len=$(echo -n "$buf" | wc -c)
+max=74994
+
+if [ $len -gt $max ]; then
+    echo "Input is too large for OSC 52" >&2
+else
     esc="\033]52;c;$(echo -n "$buf" | base64)\a"
     printf "$esc"
-}
-
-is_ssh() {
-    [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] || [ -n "$SSH_CONNECTION" ] && return 0 || return 1
-}
-
-if is_ssh; then
-    echo "copy over ssh"
-    osc52
-else
-    /usr/bin/pbcopy
 fi
