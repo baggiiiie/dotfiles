8b8ed466fb6a29de07cdfa48d544341e0e37c914
baggiiiie <yc@yingchaoooo.com>
2025-12-12 23:47:31 +0800

    zsh/tmux: init script

--- others/tmux-init-sesh.sh
+++ others/tmux-init-sesh.sh
@@ -16,7 +16,7 @@ tmux new-window -t "$session_name" -n jjui -c "$session_path"
 tmux new-window -t "$session_name" -n server -c "$session_path"
 
 # Rename the first window to "editor"
-tmux rename-window -t "$session_name:1" -c "$session_path" editor
+tmux rename-window -t "$session_name:1" editor
 
 # Switch back to the first window (editor)
 tmux select-window -t "$session_name:1"
