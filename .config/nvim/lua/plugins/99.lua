3032e0d7c5cfad41acdcb221093ed4b061a85e82
baggiiiie <yc@yingchaoooo.com>
2026-02-11 13:49:31 +0800

    nvim: update 99

--- .config/nvim/lua/plugins/99.lua
+++ .config/nvim/lua/plugins/99.lua
@@ -1,6 +1,11 @@
 return {
   "ThePrimeagen/99",
   config = function()
+    local utils = require("99.utils")
+    utils.random_file = function()
+      return string.format("/tmp/99-%d", math.floor(math.random() * 10000))
+    end
+
     local _99 = require("99")
 
     -- For logging that is to a file if you wish to trace through requests
@@ -9,15 +14,16 @@ return {
     local cwd = vim.uv.cwd()
     local basename = vim.fs.basename(cwd)
     _99.setup({
+      model = "github-copilot/claude-sonnet-4.5",
+      -- provider = _99.ClaudeCodeProvider,  -- default: OpenCodeProvider
       logger = {
         level = _99.DEBUG,
         path = "/tmp/" .. basename .. ".99.debug",
         print_on_error = true,
       },
 
-      --- A new feature that is centered around tags
+      --- Completions: #rules and @files in the prompt buffer
       completion = {
-        --- Defaults to .cursor/rules
         -- I am going to disable these until i understand the
         -- problem better.  Inside of cursor rules there is also
         -- application rules, which means i need to apply these
@@ -37,12 +43,20 @@ return {
         --- ... the other rules in that dir ...
         ---
         custom_rules = {
-          "scratch/custom_rules/",
+          vim.fn.expand("~/.config/agents/skills/"),
+        },
+
+        --- Configure @file completion (all fields optional, sensible defaults)
+        files = {
+          -- enabled = true,
+          -- max_file_size = 102400,     -- bytes, skip files larger than this
+          -- max_files = 5000,            -- cap on total discovered files
+          -- exclude = { ".env", ".env.*", "node_modules", ".git", ... },
         },
 
         --- What autocomplete do you use.  We currently only
         --- support cmp right now
-        -- source = "cmp", -- disabled: nvim-cmp not installed
+        source = "cmp",
       },
 
       --- WARNING: if you change cwd then this is likely broken
@@ -59,10 +73,6 @@ return {
       },
     })
 
-    -- Create your own short cuts for the different types of actions
-    vim.keymap.set("n", "<leader>9f", function()
-      _99.fill_in_function()
-    end)
     -- take extra note that i have visual selection only in v mode
     -- technically whatever your last visual selection is, will be used
     -- so i have this set to visual mode so i dont screw up and use an
@@ -72,19 +82,11 @@ return {
     -- so just prepare for it now
     vim.keymap.set("v", "<leader>9v", function()
       _99.visual()
-    end)
+    end, { desc = "visual" })
 
     --- if you have a request you dont want to make any changes, just cancel it
     vim.keymap.set("v", "<leader>9s", function()
       _99.stop_all_requests()
-    end)
-
-    --- Example: Using rules + actions for custom behaviors
-    --- Create a rule file like ~/.rules/debug.md that defines custom behavior.
-    --- For instance, a "debug" rule could automatically add printf statements
-    --- throughout a function to help debug its execution flow.
-    vim.keymap.set("n", "<leader>9fd", function()
-      _99.fill_in_function()
     end)
   end,
 }
