441f85cd13f839726d894353f797b90c2a917284
baggiiiie <yc@yingchaoooo.com>
2026-02-11 22:17:21 +0800

    fish: replace oh-my-posh with custom fish_prompt
    Use a lightweight fish_prompt that shows time, path, and jj/git VCS
    info directly, instead of relying on oh-my-posh. The jj prompt displays
    change ID, description, bookmarks, and empty status.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,30 +1,33 @@
-function fish_prompt --description 'Write out the prompt'
+function fish_prompt
     set -l last_status $status
-    set -l normal (set_color normal)
-    set -l status_color (set_color brgreen)
-    set -l cwd_color (set_color $fish_color_cwd)
-    set -l vcs_color (set_color brpurple)
-    set -l prompt_status ""
-
-    # Since we display the prompt on a new line allow the directory names to be longer.
-    set -q fish_prompt_pwd_dir_length
-    or set -lx fish_prompt_pwd_dir_length 0
-
-    # Color the prompt differently when we're root
-    set -l suffix '❯'
-    if functions -q fish_is_root_user; and fish_is_root_user
-        if set -q fish_color_cwd_root
-            set cwd_color (set_color $fish_color_cwd_root)
-        end
-        set suffix '#'
-    end
-
-    # Color the prompt in red on error
+    set -l stat
     if test $last_status -ne 0
-        set status_color (set_color $fish_color_error)
-        set prompt_status $status_color "[" $last_status "]" $normal
+        set stat (set_color brred)"[$last_status]"(set_color normal)
+        printf '%s ' $stat
+    else
     end
 
-    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
-    echo -n -s $status_color $suffix ' ' $normal
+    set_color brcyan
+    printf '%s ' (gdate +"%T")
+    set_color brblue
+    printf '%s' (prompt_pwd --full-length-dirs 4)
+
+    # VCS info: jj if available, otherwise git
+    if jj root --quiet >/dev/null 2>&1
+        set -l jj_info (jj log -r @ -n 1 --no-graph --color always --ignore-working-copy -T 'surround("(", ")", separate(" ", change_id.shortest(), description.first_line(), bookmarks.join(" "), if(empty, "(empty)")))' 2>/dev/null)
+        if test -n "$jj_info"
+            set_color normal
+            printf ' %s' "$jj_info"
+        end
+    else
+        set_color normal
+        printf ' %s' (fish_vcs_prompt)
+    end
+
+    set_color normal
+
+    echo
+
+    printf '↪ '
+    set_color normal
 end
