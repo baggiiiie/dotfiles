1a765d3a3ef1f1697366fb6e02d18658e76083ba
baggiiiie <yc@yingchaoooo.com>
2026-02-19 10:25:14 +0800

    jjui, combining utils

--- .config/jjui/plugins/bookmark.lua
+++ .config/jjui/plugins/bookmark.lua
@@ -1,6 +1,8 @@
+local utils = require("plugins.bookmark_utils")
 local M = {}
+
 function M.setup(config)
-	config.action("bookmark2", function()
+	config.action("bookmark", function()
 		bookmark()
 	end, {
 		desc = "bookmark",
@@ -10,49 +12,25 @@ function M.setup(config)
 end
 
 function bookmark()
-	-- Get the current revision's change_id
-	template = 'if(self.present() && !remote, self.name() ++ "\n", "")'
 	local change_id = context.change_id()
 	if not change_id then
 		flash("No revision selected")
 		return
 	end
 
-	-- Get bookmarks on this revision
-	local output = jj("bookmark", "list", "-r", change_id, "-T", template)
-	local bookmarks = {}
-	for line in output:gmatch("[^\n]+") do
-		if line ~= "" then
-			table.insert(bookmarks, line)
-		end
-	end
+	local bookmarks = utils.get_bookmarks(change_id)
 
-	local function bookmark_operations(bookmark)
+	local function show_ops(b)
 		local op = choose({
-			options = { "rename " .. bookmark, "delete " .. bookmark, "forget " .. bookmark },
-			title = "Select operation: ",
+			options = { "rename " .. b, "delete " .. b, "forget " .. b },
+			title = "Select operation for " .. b .. ": ",
 		})
-		if not op then
-			return
-		end
-
-		if string.find(op, "rename") then
-			local new_name = input({ title = "New bookmark name", value = bookmark })
-			if new_name and new_name ~= "" then
-				jj("bookmark", "rename", bookmark, new_name)
-				revisions.refresh()
-			end
-		elseif string.find(op, "delete") then
-			jj("bookmark", "delete", bookmark)
-			revisions.refresh()
-		elseif string.find(op, "forget") then
-			jj("bookmark", "forget", bookmark)
-			revisions.refresh()
+		if op then
+			utils.perform_op(op, b, change_id)
 		end
 	end
 
 	if #bookmarks == 0 then
-		-- No bookmarks: show create/move menu
 		local action = choose({ options = { "create bookmark", "move bookmark" }, title = "No bookmarks on revision" })
 		if not action then
 			return
@@ -65,37 +43,22 @@ function bookmark()
 				revisions.refresh()
 			end
 		elseif action == "move bookmark" then
-			-- Get all bookmarks to move one here
-			local all_output = jj("bookmark", "list", "-T", template)
-			local all_bookmarks = {}
-			for line in all_output:gmatch("[^\n]+") do
-				if line ~= "" then
-					table.insert(all_bookmarks, line)
-				end
-			end
+			local all_bookmarks = utils.get_bookmarks()
 			if #all_bookmarks == 0 then
 				flash("No bookmarks to move")
 				return
 			end
-			local to_move = choose({ options = all_bookmarks, { title = "Select bookmark to move" }, filter = true })
+			local to_move = choose({ options = all_bookmarks, title = "Select bookmark to move", filter = true })
 			if to_move then
-				local move_res, err = jj("bookmark", "move", to_move, "--to", change_id, "--allow-backwards")
-				if err ~= nil then
-					flash("Error moving bookmark: " .. err)
-				else
-					flash("Moved bookmark " .. move_res)
-					revisions.refresh()
-				end
+				utils.perform_op("move", to_move, change_id)
 			end
 		end
 	elseif #bookmarks == 1 then
-		-- Single bookmark: show operations directly
-		bookmark_operations(bookmarks[1])
+		show_ops(bookmarks[1])
 	else
-		-- Multiple bookmarks: let user pick one first
 		local selected = choose({ options = bookmarks, title = "Select bookmark" })
 		if selected then
-			bookmark_operations(selected)
+			show_ops(selected)
 		end
 	end
 end
---
+++ .config/jjui/plugins/bookmark_utils.lua
@@ -0,0 +1,56 @@
+local unpack = table.unpack or unpack
+local M = {}
+
+M.template = 'if(self.present() && !remote, self.name() ++ "\n", "")'
+
+function M.get_bookmarks(rev)
+	local args = { "bookmark", "list", "-T", M.template }
+	if rev then
+		table.insert(args, "-r")
+		table.insert(args, rev)
+	end
+
+	local output = jj(unpack(args))
+	local bookmarks = {}
+	for line in output:gmatch("[^\n]+") do
+		if line ~= "" then
+			table.insert(bookmarks, line)
+		end
+	end
+	return bookmarks
+end
+
+function M.perform_op(op, bookmark, current_change_id)
+	if string.find(op, "rename") then
+		local new_name = input({ title = "New bookmark name", value = bookmark })
+		if new_name and new_name ~= "" then
+			jj("bookmark", "rename", bookmark, new_name)
+			revisions.refresh()
+		end
+	elseif string.find(op, "delete") then
+		jj("bookmark", "delete", bookmark)
+		revisions.refresh()
+	elseif string.find(op, "forget") then
+		jj("bookmark", "forget", bookmark)
+		revisions.refresh()
+	elseif string.find(op, "move") then
+		local target = current_change_id
+		if not target or string.find(op, "move " .. bookmark) then
+			target = input({ title = "Move bookmark to (change ID)", value = current_change_id or "" })
+		end
+
+		if target and target ~= "" then
+			local _, err = jj("bookmark", "move", bookmark, "--to", target, "--allow-backwards")
+			if err ~= nil then
+				flash("Error moving bookmark: " .. err)
+			else
+				flash("Moved bookmark " .. bookmark .. " to " .. target)
+				revisions.refresh()
+			end
+		end
+	elseif string.find(op, "show revision") then
+		revset.set("trunk()::" .. bookmark)
+	end
+end
+
+return M
